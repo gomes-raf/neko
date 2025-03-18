8fc04e51733abd735f433a266bc71c76da404f05
Rafael Gomes <rafael.gomes@sou.inteli.edu.br>
2025-03-18 11:07:25 -0300

    Update base.js

--- base.js
+++ base.js
@@ -1,8 +1,12 @@
 //Adiciona configurações que transformam o jogo.
 const config = {
     type: Phaser.AUTO,
+    scale: {
+    mode:Phaser.Scale.FIT,
     width: 900, //Largura da tela
     height: 600, //Altura da tela
+    autoCenter: Phaser.Scale.NO_CENTER,
+    },
 //Define informações sobre a física do jogo
     physics: {
         default: 'arcade', //Tipo de física
